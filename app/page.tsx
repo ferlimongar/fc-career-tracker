e404c32dfbae0753d608a86f6cea9d9acddd2cc7
Fernando Limon <ferlimongar@gmail.com>
2026-07-12 16:39:18 -0700

    Initial Next.js project setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-[#0B0D10] text-white">
+      <h1 className="text-5xl font-bold p-10">
+        FC Career Tracker
+      </h1>
+    </main>
+  );
+}
